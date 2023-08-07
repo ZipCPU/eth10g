b6d8ae2fdf2440a47743fd8631c6db9fe8522d10
ZipCPU <dgisselq@ieee.org>
2023-08-07 08:00:32 -0400

    NEW: Full proofs for pkt2p64b and p66btxgears (not tested)

--- rtl/net/p66btxgears.v
+++ rtl/net/p66btxgears.v
@@ -96,9 +96,15 @@ module	p66btxgears // #()
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 `ifdef	FORMAL
-// Note: this is (currently) only a partial proof
-	reg	f_past_valid;
-	// assign	f_count = { r_count, 1'b0 } + 64;
+	reg	[7:0]	f_count;
+	reg		f_past_valid;
+	(* anyconst *)	reg	[18:0]	fc_posn
+	(* anyconst *)	reg	[65:0]	fc_data;
+	(* anyconst *)	reg		fc_check;
+	reg	[18:0]	fs_posn, fg_posn, f_offset;
+	reg	[127:0]	f_shifted;
+	reg	[65:0]	fc_shift, f_mask;
+	reg	[3:0]	f_pipe;
 
 	initial	f_past_valid = 0;
 	always @(posedge i_clk)
@@ -107,6 +113,12 @@ module	p66btxgears // #()
 	always @(*)
 	if (!f_past_valid)
 		assume(i_reset);
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Gearbox
+	// {{{
+	always @(*)
+		f_count = shift;
 
 	always @(*)
 	if (!i_reset)
@@ -123,16 +135,94 @@ module	p66btxgears // #()
 	always @(posedge i_clk)
 	if (!i_reset && !$past(i_reset) && !$past(i_ready))
 	begin
-		assert($stable(r_count));
+		assert($stable(f_count));
 		// assert($stable(pre_ready));
 		assert($stable(o_data));
 	end
 
 	always @(posedge i_clk)
-	if (!i_reset && !$past(i_reset) && $past(r_count >= 64))
+	if (!i_reset && !$past(i_reset) && $past(f_count >= 64))
 	begin
-		assert(r_count >= 64);
+		assert(f_count >= 64);
 	end
+	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Contract
+	// {{{
+	// Start ... by counting.
+
+	always @(posedge i_clk)
+	if (i_reset)
+		f_pipe <= 0;
+	else
+		f_pipe <= { f_pipe[2:0], 1'b1 };
+
+	always @(posedge i_clk)
+	if (i_reset)
+		fs_posn <= 0;
+	else if (S_READY)
+		fs_posn <= fs_posn + 66;
+
+	always @(posedge i_clk)
+	if (i_reset)
+		fg_posn <= 0;
+	else if (S_READY)
+		fg_posn <= fs_posn - shift;
+	else if (i_ready)
+		fg_posn <= fg_posn+64;
+
+	// 
+	// Without a little help, it's not clear we'd ever land on one of
+	// our items.  Therefore, let's assume fc_posn becomes valid at some
+	// point, and doesn't get skipped.
+	always @(*)
+	if (fs_posn <= fc_posn && fc_posn < fs_posn+66)
+		assume(fc_data == fs_posn);
+
+	// Finally, ASSUME the given data when we hit the given count.
+	always @(*)
+	if (fc_check && fs_posn == fc_posn)
+		assume(fc_data == S_DATA);
+
+	always @(*)
+	if (fc_posn > fg_posn)
+		f_offset = fc_posn - fg_posn;
+	else
+		f_offset = 0;
+
+	always @(*)
+		f_shifted = (gearbox >> f_offset);
+	always @(*)
+	if (fc_posn >= fg_posn)
+		fc_shift = fc_data;
+	else
+		fc_shift = fc_data >> (fg_posn - fc_posn);
+
+	always @(*)
+	if (fc_posn+66 < fg_posn && fg_posn > fc_posn)
+		f_mask = {(66){1'b1}} >> (fg_posn-fc_posn);
+	else // if (fc_posn >= fg_posn)
+		f_mask = {(66){1'b1}};
+
+	always @(posedge i_clk)
+	if (!i_reset && fc_check && fg_posn <= fc_posn+66
+			&& fc_posn < fg_posn + f_count && f_pipe[2])
+		assert(0 == ((fc_shift ^ f_shifted[65:0]) & f_mask));
+
+	// Let's just make sure we can do this properly ...
+	always @(*)
+	if (!i_reset)
+	begin
+		cover(fc_posn == 132 && fg_posn > 132 + 130);
+		cover(fc_posn == 198 && fg_posn > 198 + 130);
+		cover(fc_posn == 264 && fg_posn > 264 + 130);
+		cover(fc_posn == 330 && fg_posn > 330 + 130);
+		cover(fc_posn == 396 && fg_posn > 396 + 130);
+		cover(fc_posn == 462 && fg_posn > 462 + 130);
+	end
+
+	// }}}
 `endif
 // }}}
 endmodule
--- rtl/net/pkt2p64b.v
+++ rtl/net/pkt2p64b.v
@@ -95,14 +95,23 @@ module	pkt2p64b (
 	(* ASYNC_REG = "TRUE" *) reg	[1:0]	r_local_fault_pipe,
 						r_remote_fault_pipe;
 	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Fault CDC and stretching
+	// {{{
 
+	// Local faults
+	// {{{
 	always @(posedge TX_CLK)
 	if (!S_ARESETN)
 		{ r_local_fault, r_local_fault_pipe } <= 0;
 	else
 		{ r_local_fault, r_local_fault_pipe }
 				<= { r_local_fault_pipe, i_local_fault };
+	// }}}
 
+	// Remote faults -> stretch_fault
+	// {{{
 	always @(posedge TX_CLK)
 	if (!S_ARESETN)
 		{ r_remote_fault, r_remote_fault_pipe } <= 0;
@@ -126,6 +135,8 @@ module	pkt2p64b (
 			stretch_counter <= stretch_counter - 1;
 		stretch_fault <= (stretch_counter > 1);
 	end
+	// }}}
+	// }}}
 	
 	initial	r_ready = 1'b0;
 	initial	TXDATA  = P_IDLE;
@@ -240,4 +251,268 @@ module	pkt2p64b (
 	endcase
 
 	assign	S_READY = r_ready && (TXREADY || flushing);
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+// Formal properties
+// {{{
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+`ifdef	FORMAL
+	reg	f_past_valid;
+	wire	[10:0]	fs_word;
+	wire	[12-1:0]	fs_packets;
+
+	initial	f_past_valid = 1'b0;
+	always @(posedge TX_CLK)
+		f_past_valid <= 1'b1;
+
+	always @(*)
+	if (!f_past_valid)
+		assume(!S_ARESETN);
+
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Input stream properties
+	// {{{
+	faxin_slave #(
+		.DATA_WIDTH(64)
+	) faxin (
+		// {{{
+		.S_AXI_ACLK(TX_CLK), .S_AXI_ARESETN(S_ARESETN),
+		//
+		.S_AXIN_VALID(S_VALID),
+		.S_AXIN_READY(S_READY),
+		.S_AXIN_DATA(S_DATA),
+		.S_AXIN_BYTES(S_BYTES),
+		.S_AXIN_LAST(S_LAST),
+		.S_AXIN_ABORT(S_ABORT),
+		//
+		.f_stream_word(fs_word), .f_packets_rcvd(fs_packets)
+		// }}}
+	);
+
+	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Output 66b protocol
+	// {{{
+	always @(posedge TX_CLK)
+	if (S_ARESETN && !$past(S_ARESETN))
+	begin
+		assert(TXDATA == P_IDLE);
+	end else if (!$past(TXREADY))
+		assert($stable(TXDATA));
+	else if (($past(TXDATA[1:0]) == SYNC_CONTROL)
+					&&($past(TXDATA) != P_START))
+		assert(TXDATA[1:0] == SYNC_CONTROL);
+
+	always @(posedge TX_CLK)
+	if (S_ARESETN && !$past(S_ARESETN))
+	begin
+		assert(TXDATA == P_IDLE);
+	end
+
+	always @(*)
+	if (S_ARESETN)
+	begin
+		assert(^TXDATA[1:0]);
+		if (TXDATA[1:0] == SYNC_CONTROL)
+		begin
+			case(TXDATA[9:2])
+			8'h1e: assert(TXDATA == P_IDLE);
+			8'h55: assert(TXDATA == P_FAULT);
+			8'h78: assert(TXDATA == P_START);
+			8'h87: assert(TXDATA == P_LAST);
+			//
+			8'h99: assert(TX_DATA[65:18] == 0);
+			8'haa: assert(TX_DATA[65:26] == 0);
+			8'hb4: assert(TX_DATA[65:34] == 0);
+			8'hcc: assert(TX_DATA[65:42] == 0);
+			8'hd2: assert(TX_DATA[65:50] == 0);
+			8'he1: assert(TX_DATA[65:58] == 0);
+			8'hff: begin end
+			default: assert(0);
+			endcase
+		end
+	end
+	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Contract
+	// {{{
+	(* anyconst *)	reg	fc_check;
+	(* anyconst *)	reg	[10:0]	fc_posn;
+	(* anyconst *)	reg	[63:0]	fc_data;
+	(* anyconst *)	reg	[2:0]	fc_bytes;
+	(* anyconst *)	reg		fc_last, fnvr_local;
+	reg		f_eop;
+	reg	[10:0]	fpkt_word;
+
+	// Assume a known/valid input, with a known word of data
+	// {{{
+	// Note we have to be careful to caveat all of our assumptions to
+	// only take hold when fc_check is true.  This is the case where we
+	// assume an uninterrupted packet input.  It may not truly be the
+	// case, however.  Therefore, we must allow for !fc_check, and only
+	// make these assumptions when fc_check is true.
+	always @(*)
+	if (!fc_last)
+		assume(fc_bytes == 0);
+
+	// Incoming stream *MUST* remain valid for a packet to be successful
+	always @(posedge TX_CLK)
+	if (S_ARESETN && $past(S_ARESETN) && fc_check
+					&& $past(S_VALID && !S_LAST))
+		assume(S_VALID);
+
+	always @(*)
+	if (S_ARESETN && $past(S_ARESETN) && fc_check && S_VALID)
+		assume(!S_ABORT);
+
+	always @(*)
+	if (S_ARESETN && fc_check && fs_posn < fc_posn == fs_posn && S_VALID)
+		assume(!S_LAST);
+
+	always @(*)
+	if (S_ARESETN && fc_check && fc_posn == fs_posn && S_VALID)
+	begin
+		assume(S_DATA  == fc_data);
+		assume(S_BYTES == fc_bytes);
+		assume(S_LAST  == fc_last);
+	end
+
+	always @(*)
+	if (fnvr_local)
+		assume(!i_local_fault);
+
+	// This won't work if faults take place, so let's assume no faults
+	// during our special check packet
+	always @(*)
+	if (fc_check)
+		assume(fpkt_word == 0 || !r_remote_fault);
+	// }}}
+
+	always @(*)
+	if (TXDATA[1:0] != S_CONTROL)
+		f_eop = 1'b0;
+	else case(TXDATA[9:2])
+		8'h99: f_eop = 1'b1;
+		8'haa: f_eop = 1'b1;
+		8'hb4: f_eop = 1'b1;
+		8'hcc: f_eop = 1'b1;
+		8'hd2: f_eop = 1'b1;
+		8'he1: f_eop = 1'b1;
+		8'hff: f_eop = 1'b1;
+		P_LAST[9:2]: f_eop = 1'b1;
+		default: f_eop = 1'b0;
+	endcase
+
+	always @(posedge TX_CLK)
+	if (!S_ARESETN || flushing)
+	begin
+		fpkt_word <= 0;
+	end else if (TXREADY)
+	begin
+		if (TXDATA == P_START)
+			fpkt_word <= 1;
+		else if (TXDATA[1:0] == SYNC_DATA)
+			fpkt_word <= fpkt_word + 1;
+		else if (f_eop)
+			fpkt_word <= 0;
+	end
+
+	always @(posedge TX_CLK)
+	if (S_ARESETN && !flushing)
+	begin
+		if (fs_word > 0)
+			assert(fs_word == fpkt_word);
+		else if (!f_eop)
+			assert(fpkt_word == 0);
+	end
+
+	always @(*)
+	if (fc_check && fpkt_word == fc_posn && TXDATA[1:0] == SYNC_DATA)
+	begin
+		assert(TXDATA[65:0] == fc_data);
+		assert(!fc_last || fc_bytes == 0);
+	end else if (fc_check && fpkt_word == fc_posn
+					&& TXDATA[1:0] == SYNC_CONTROL)
+	begin
+		assert(fc_last && fc_bytes != 0);
+		case(TXDATA[9:2])
+		8'h99: assert(TX_DATA[17:10] == fc_data[7:0]);
+		8'haa: assert(TX_DATA[25:10] == fc_data[15:0]);
+		8'hb4: assert(TX_DATA[33:10] == fc_data[23:0]);
+		8'hcc: assert(TX_DATA[41:10] == fc_data[31:0]);
+		8'hd2: assert(TX_DATA[49:10] == fc_data[39:0]);
+		8'he1: assert(TX_DATA[57:10] == fc_data[47:0]);
+		8'hff: assert(TX_DATA[65:10] == fc_data[55:0]);
+		default: begin end
+		end
+	end else if (fc_check && fpkt_word == fc_posn + 1)
+	begin
+		assert(f_last && f_bytes == 0);
+		assert(f_eop && TXDATA == P_LAST);
+	end else if (fc_check && fc_posn > 0)
+		assert(fpkt_word < fc_posn);
+
+	always @(*)
+	if (S_ARESETN && fnvr_local)
+		assert(!r_local_fault);
+
+	always @(*)
+	if (S_ARESETN && fc_check && fpkt_word > 0)
+	begin
+		assert(!flushing);
+		assert(!stretch_fault);
+	end
+
+	always @(*)
+	if (S_ARESETN && !flushing)
+	case(state)
+	TX_IDLE: assert(fs_word == 0);
+	TX_DATA: assert(fs_word == fpkt_word);
+	TX_LAST: assert(fs_word == 0);
+	TX_PAUSE: assert(fs_word == 0);
+	endcase
+	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// Cover
+	// {{{
+
+	always @(posedge TX_CLK)
+	if (S_ARESETN && $past(S_ARESETN))
+		cover(TXDATA[1:0] == SYNC_DATA);
+
+	always @(posedge TX_CLK)
+	if (S_ARESETN && $past(S_ARESETN) && TXDATA[1:0] == SYNC_CONTROL)
+	begin
+		case(TXDATA[9:2])
+		8'h1e: cover(1);
+		8'h55: cover(1);
+		8'h78: cover(1);
+		8'h87: cover(1);
+		//
+		8'h99: cover(1);
+		8'haa: cover(1);
+		8'hb4: cover(1);
+		8'hcc: cover(1);
+		8'hd2: cover(1);
+		8'he1: cover(1);
+		8'hff: cover(1);
+		endcase
+	end
+
+	// }}}
+	////////////////////////////////////////////////////////////////////////
+	//
+	// "Careless" assumptions
+	// {{{
+	// }}}
+`endif
+// }}}
 endmodule
