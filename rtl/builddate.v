fb18b7622d05a68c5534b3a9c0d89e0dfcdc196c
ZipCPU <dgisselq@ieee.org>
2025-10-03 14:10:57 -0400

    Adding the date stamp file to the repo

---
+++ rtl/builddate.v
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: 	builddate.v
+// {{{
+// Project:	10Gb Ethernet switch
+//
+// Purpose:	This file records the date of the last build.  Running "make"
+//		in the main directory will create this file.  The `define found
+//	within it then creates a version stamp that can be used to tell which
+//	configuration is within an FPGA and so forth.
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+// }}}
+// Copyright (C) 2023-2025, Gisselquist Technology, LLC
+// {{{
+// This file is part of the ETH10G project.
+//
+// The ETH10G project contains free software and gateware, licensed under the
+// terms of the 3rd version of the GNU General Public License as published by
+// the Free Software Foundation.
+//
+// This project is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the 1000 4 20 24 27 30 46 120 131 132 144 1000ROOT)/doc directory.  Run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+// }}}
+// License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
+//		http://www.gnu.org/licenses/gpl.html
+//
+////////////////////////////////////////////////////////////////////////////////
+// }}}
+`ifndef	DATESTAMP
+`define DATESTAMP 32'h20250429
+`define BUILDTIME 32'h00184822
+`endif
+//
