4870768aa6892b53055d84ed6561da41e291e0a0
ZipCPU <dgisselq@ieee.org>
2026-07-25 14:43:32 -0400

    FIX:  in builddate.v was an escape in PL, not TXT

--- rtl/builddate.v
+++ rtl/builddate.v
@@ -28,7 +28,7 @@
 // for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with this program.  (It's in the 1000 4 20 24 27 30 46 120 131 132 144 1000ROOT)/doc directory.  Run make with no
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
 // }}}
@@ -40,6 +40,6 @@
 // }}}
 `ifndef	DATESTAMP
 `define DATESTAMP 32'h20260725
-`define BUILDTIME 32'h00122609
+`define BUILDTIME 32'h00144242
 `endif
 //
