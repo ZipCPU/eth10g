a32ad27273b9ae70772e1ebc9491a5732154cc66
ZipCPU <dgisselq@ieee.org>
2026-08-24 15:53:37 -0400

    NEW: S/W to control a SATA RX Scope
    Need to "see" what's being received
    NEW: Updated status chart

---
+++ sw/host/satarxscope.cpp
@@ -0,0 +1,104 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename:	sw/host/satarxscope.cpp
+// {{{
+// Project:	10Gb Ethernet switch
+//
+// Purpose:	
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+// }}}
+// Copyright (C) 2023-2026, Gisselquist Technology, LLC
+// {{{
+// This file is part of the ETH10G project.
+//
+// The ETH10G project contains free software and gateware, licensed under the
+// terms of the 3rd version of the GNU General Public License as published by
+// the Free Software Foundation.
+//
+// This project is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+// }}}
+// License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
+//		http://www.gnu.org/licenses/gpl.html
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// }}}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <ctype.h>
+#include <string.h>
+#include <signal.h>
+#include <assert.h>
+#include <vector>
+
+#include "regdefs.h"
+#include "devbus.h"
+#include "scopecls.h"
+
+#ifndef	R_SATARXSCOPE
+int main(int argc, char **argv) {
+	printf("This design was not built with a SATA-RX scope within it.\n");
+}
+#else
+
+#define	WBSCOPE		R_SATARXSCOPE
+#define	WBSCOPEDATA	R_SATARXSCOPED
+
+DEVBUS	*m_fpga;
+void	closeup(int v) {
+	m_fpga->kill();
+	exit(0);
+}
+
+class	SATASCOPE : public SCOPE {
+public:
+
+	SATASCOPE(DEVBUS *fpga, unsigned addr, bool vecread = true)
+		: SCOPE(fpga, addr, true, vecread) {};
+	~SATASCOPE(void) {}
+
+	virtual	void	decode(DEVBUS::BUSW val) const {
+		// {{{
+		// }}}
+	}
+
+	virtual	void	define_traces(void) {
+		register_trace("i_rx_valid",     1, 30);
+		register_trace("i_rx_primitive", 1, 29);
+		register_trace("i_rx_data",      1, 29);
+	}
+};
+
+int main(int argc, char **argv) {
+	m_fpga = connect_devbus(NULL);
+
+	signal(SIGSTOP, closeup);
+	signal(SIGHUP, closeup);
+
+	SATASCOPE *scope = new SATASCOPE(m_fpga, WBSCOPE);
+	scope->set_clkfreq_hz(37500000);
+	if (!scope->ready()) {
+		printf("Scope is not yet ready:\n");
+		scope->decode_control();
+	} else {
+		scope->print();
+		scope->writevcd("satarx.vcd");
+	}
+}
+
+#endif
